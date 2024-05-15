3549690781133a37e8ac1069869e35d129ca163f
TimotheChiesi <timothe.chiesi@etu.mines-ales.fr>
2024-05-15 13:33:47 +0200

    Initial commit

---
+++ Assets/Pixel Adventure 1/Assets/Scripts/FruitManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FruitManager : MonoBehaviour
+{ 
+    private void Update()
+    {
+        AllFruitsCollected();
+    }
+
+    public void AllFruitsCollected()
+    {
+        if (transform.childCount == 0)
+        {
+            Debug.Log("No quedan frutas");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+    }
+}
