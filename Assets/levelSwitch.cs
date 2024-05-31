8df14071cd89a2e4eb36c0d7a6df9882f7ed38e9
allani fadia <allanifadia@gmail.com>
2024-05-31 09:08:25 +0200

    Water Girl Door

--- Assets/levelSwitch.cs
+++ Assets/levelSwitch.cs
@@ -1,22 +1,56 @@
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Switch : MonoBehaviour
 {
-     private string nextLevelName = "Level_1";
+    private string nextLevelName;
+    private int currentLevel;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    //Recuperer le num du level
+    private int GetLevelNumberFromSceneName(string sceneName)
     {
-        Debug.Log("Trigger entered by: " + collision.name);
-        if (collision.CompareTag("WaterGirl"))
+       
+        Match match = Regex.Match(sceneName, @"\d+");
+        if (sceneName == "Level_Intro")
         {
-            Debug.Log("WaterGirl detected, changing level...");
-            ChangeLevel();
+            return 0;
+        }
+        if (match.Success)
+        {
+            return int.Parse(match.Value);
+        }
+        else
+        {
+            Debug.LogError("Failed to parse level number from scene name: " + sceneName);
+            return 0; // Default to level 1 if parsing fails
         }
     }
 
-    private void ChangeLevel()
+    private void OnTriggerEnter2D(Collider2D collision)
+      {
+        Debug.Log("Trigger entered by: " + collision.name);
+        if (collision.CompareTag("WaterGirl"))
+          {
+            Debug.Log("WaterGirl detected, changing level...");
+            ChangeLevel();
+          }
+        }
+
+    void Start()
     {
+        // Extract the current level number from the scene name
+        currentLevel = GetLevelNumberFromSceneName(SceneManager.GetActiveScene().name);
+    }
+
+
+
+    private void ChangeLevel()
+    {   if(currentLevel == 0)
+        {
+            nextLevelName = "Level_1";
+        }
+        nextLevelName= "Level_" + (currentLevel + 1).ToString();
         if (string.IsNullOrEmpty(nextLevelName))
         {
             Debug.LogError("Next level name is empty or null!");
